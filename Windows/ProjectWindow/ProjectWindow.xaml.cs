14f266a34574495dd910637d6c09f7e30e993665
Lexaik <ka34enko@gmail.com>
2024-12-01 22:27:19 +0400

    Создал проект

---
+++ Windows/ProjectWindow/ProjectWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TOP_Planner.Windows.ProjectWindow;
+
+public partial class ProjectWindow : Window
+{
+    public ProjectWindow()
+    {
+        InitializeComponent();
+        Today.Content = DateTime.Now.ToString("dd");
+    }
+}
