064dffe6da26642c6728ff5fbfc3c2bdb7c224f9
Lexaik <ka34enko@gmail.com>
2024-12-08 12:26:31 +0400

    Добавил стили из работы на занятиях и контрол

---
+++ Components/OutputControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace TOP_Planner.Components;
+
+public partial class OutputControl : UserControl
+{
+    public OutputControl()
+    {
+        InitializeComponent();
+    }
+}
--- Windows/ProjectWindow/ProjectWindow.xaml.cs
+++ Windows/ProjectWindow/ProjectWindow.xaml.cs
@@ -7,6 +7,6 @@ public partial class ProjectWindow : Window
     public ProjectWindow()
     {
         InitializeComponent();
-        Today.Content = DateTime.Now.ToString("dd");
+        
     }
 }
